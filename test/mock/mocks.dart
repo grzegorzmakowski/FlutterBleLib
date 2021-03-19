8b6a6140f0fa62bda5a694f8119d3bd1997b8fc0
Grzegorz Makowski <makowski.grzegorz.developer@gmail.com>
2021-03-19 12:57:29 +0100

    Refactor

--- test/mock/mocks.dart
+++ test/mock/mocks.dart
@@ -1,17 +1,8 @@
-
-import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:flutter_ble_lib/flutter_ble_lib.dart';
 import 'package:flutter_ble_lib/src/_managers_for_classes.dart';
 
-
-@GenerateMocks([
-  // ManagerForService,
-  // ManagerForCharacteristic,
-  // ManagerForDescriptor,
-  // Service,
-  // Characteristic
-], customMocks: [
+@GenerateMocks([], customMocks: [
   MockSpec<Peripheral>(returnNullOnMissingStub: true),
   MockSpec<ManagerForService>(returnNullOnMissingStub: true),
   MockSpec<ManagerForCharacteristic>(returnNullOnMissingStub: true),
@@ -19,20 +10,4 @@ import 'package:flutter_ble_lib/src/_managers_for_classes.dart';
   MockSpec<Service>(returnNullOnMissingStub: true),
   MockSpec<Characteristic>(returnNullOnMissingStub: true)
 ])
-class Yolo {
-
-}
-
-
-// class ManagerForServiceMock extends Mock implements ManagerForService {}
-//
-// class ManagerForCharacteristicMock extends Mock
-//     implements ManagerForCharacteristic {}
-//
-// class ManagerForDescriptorMock extends Mock implements ManagerForDescriptor {}
-//
-// class ServiceMock extends Mock implements Service {}
-//
-// class PeripheralMock extends Mock implements Peripheral {}
-//
-// class CharacteristicMock extends Mock implements Characteristic {}
+class Lorem {}
